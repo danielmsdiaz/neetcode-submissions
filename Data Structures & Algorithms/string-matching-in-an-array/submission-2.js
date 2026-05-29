66e3440fecc2d5b5ec4b7d3e84b1b9ead87c913f
Daniel Diaz <59071751+danielmsdiaz@users.noreply.github.com>
2026-05-29 11:37:25 -0300

    Add: string-matching-in-an-array - submission-2

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-2.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @return {string[]}
+     */
+    stringMatching(words) {
+        words.sort((a, b) => a.length - b.length);
+
+        const result = [];
+
+        for (let i = 0; i < words.length; i++) {
+            for (let j = i + 1; j < words.length; j++) {
+                if (words[j].includes(words[i])) {
+                    result.push(words[i]);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
