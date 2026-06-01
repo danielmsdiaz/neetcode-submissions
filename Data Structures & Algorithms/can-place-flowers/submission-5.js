365b4d81357fd0215cd6469413989a8a8e8a6054
Daniel Diaz <59071751+danielmsdiaz@users.noreply.github.com>
2026-06-01 12:23:18 -0300

    Add: can-place-flowers - submission-5

---
+++ Data Structures & Algorithms/can-place-flowers/submission-5.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} flowerbed
+     * @param {number} n
+     * @return {boolean}
+     */
+    canPlaceFlowers(flowerbed, n) {
+        let can = 0;
+        
+        for (let i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] === 0 && flowerbed[i - 1] !== 1 && flowerbed[i + 1] !== 1) {
+                can++;
+                flowerbed[i] = 1;
+            }
+        }
+
+        return can >= n;
+    }
+}
